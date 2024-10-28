ca8c7a4479b7cd190c6f73ce515a68c0089a7f16
Mihail <mihail.negulet@gmail.com>
2024-10-28 16:56:51 +0200

    profile updated

---
+++ script.js
@@ -0,0 +1,4 @@
+// Derulează la secțiunea Despre mine
+document.querySelector(".scroll-button").addEventListener("click", () => {
+    document.getElementById("about").scrollIntoView({ behavior: "smooth" });
+});
